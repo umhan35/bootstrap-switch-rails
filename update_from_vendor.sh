dfaffc8a985dc54d5d5e0f91b3e8e88e10928d3c
Zhao Han <cx.chenghai+github@gmail.com>
2013-08-19 22:12:53 -0500

    .css to .css.scss: easier to import in a sass file & update update_fron_vendor.sh file

--- update_from_vendor.sh
+++ update_from_vendor.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# Checkout vendor repo
-echo "Cloning nostalgiaz/bootstrap-switch github repo into tmp_vendor"
-git clone https://github.com/nostalgiaz/bootstrap-switch.git tmp_vendor
-
-# Copy files
-echo "Copying bootstrap-switch.js"
-cp tmp_vendor/static/js/bootstrap-switch.js vendor/assets/javascripts/bootstrap-switch.js
-echo "Copying bootstrap-switch.css"
-cp tmp_vendor/static/stylesheets/bootstrap-switch.css vendor/assets/stylesheets/bootstrap-switch.css
-
-# Delete vendor repo
-echo "Removing cloned vendor repo"
-rm -rf tmp_vendor
+wget https://github.com/nostalgiaz/bootstrap-switch/raw/master/static/stylesheets/bootstrap-switch.css --output-document ./vendor/assets/stylesheets/bootstrap-switch.css.scss
 
 echo "Finished... You'll need to commit the changes. You should consider updating the changelog and gem version number"
