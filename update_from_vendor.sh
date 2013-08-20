067140a8c9071d7b9f53fe85b4f0c444bef589af
Zhao Han <cx.chenghai+github@gmail.com>
2013-08-19 22:43:24 -0500

    Oops, also need to get the .js file ;)

--- update_from_vendor.sh
+++ update_from_vendor.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+wget https://raw.github.com/nostalgiaz/bootstrap-switch/master/static/js/bootstrap-switch.js --output-document ./vendor/assets/javascripts/bootstrap-switch.js
+
 wget https://github.com/nostalgiaz/bootstrap-switch/raw/master/static/stylesheets/bootstrap-switch.css --output-document ./vendor/assets/stylesheets/bootstrap-switch.css.scss
 
 echo "Finished... You'll need to commit the changes. You should consider updating the changelog and gem version number"
